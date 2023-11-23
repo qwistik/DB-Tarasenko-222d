a8a5d53541a4fc1e64469d07640852ee3d260e66
qwistik <141317003+qwistik@users.noreply.github.com>
2023-11-23 13:18:23 +0100

    Update lab8.sql

--- laboratorni/lab8.sql
+++ laboratorni/lab8.sql
@@ -1,45 +1,42 @@
--- Таблиця користувачів
-CREATE TABLE users3 
+CREATE TABLE users 
 (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+    username VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    password VARCHAR(50) NOT NULL
+    
 );
 
--- Таблиця проектів
-CREATE TABLE projects3 
+CREATE TABLE projects
 (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     project_name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 );
 
--- Таблиця тасків
-CREATE TABLE tasks3 
+CREATE TABLE tasks
 (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    task_name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-    author_id INT UNSIGNED NOT NULL,
-    project_id INT UNSIGNED NOT NULL,
+    name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    project_id INT UNSIGNED,
+    author_id INT UNSIGNED,
     FOREIGN KEY (author_id) REFERENCES users(id),
     FOREIGN KEY (project_id) REFERENCES projects(id)
 );
 
--- Таблиця виконавців тасків
-CREATE TABLE task_assignees3 
+CREATE TABLE task_assignees
 (
-    task_id INT UNSIGNED NOT NULL,
-    user_id INT UNSIGNED NOT NULL,
+    task_id INT UNSIGNED,
+    user_id INT UNSIGNED,
     PRIMARY KEY (task_id, user_id),
     FOREIGN KEY (task_id) REFERENCES tasks(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
--- Таблиця файли тасків
-CREATE TABLE task_files3
+CREATE TABLE files
 (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    file_name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-    task_id INT UNSIGNED NOT NULL,
-    project_id INT UNSIGNED, -- Якщо файл додано до проекту, а не конкретного таску
+    name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    task_id INT UNSIGNED NULL,
+    project_id INT UNSIGNED NULL,
     FOREIGN KEY (task_id) REFERENCES tasks(id),
     FOREIGN KEY (project_id) REFERENCES projects(id)
 );
