6fe36e21d0bb206f612ddcd760092771c0e4aa34
qwistik <141317003+qwistik@users.noreply.github.com>
2023-11-23 13:25:48 +0100

    Update lab7.sql

--- laboratorni/lab7.sql
+++ laboratorni/lab7.sql
@@ -1,44 +1,43 @@
-CREATE TABLE news_categories 
+CREATE TABLE `category` 
 (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+    `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 );
 
-CREATE TABLE news 
+CREATE TABLE `news` 
 (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-    content TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-    author_name VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-    publication_date DATETIME NOT NULL,
-    category_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (category_id) REFERENCES news_categories(id)
+    `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `content` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+    `author` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+    `creation_date` DATETIME NOT NULL,
+    `category_id` INT UNSIGNED NOT NULL,
+    FOREIGN KEY (`category_id`) REFERENCES `category`(`id`)
 );
 
-CREATE TABLE users 
+CREATE TABLE `user` 
 (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+    `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 );
 
-CREATE TABLE comments 
+CREATE TABLE `comment` 
 (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    content TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-    creation_date DATETIME NOT NULL,
-    user_id INT UNSIGNED NOT NULL,
-    news_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (news_id) REFERENCES news(id)
+    `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `content` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+    `creation_date` DATETIME NOT NULL,
+    `user_name` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci ,
+    `news_id` INT UNSIGNED NOT NULL,
+    FOREIGN KEY (`news_id`) REFERENCES `news`(`id`) 
 );
 
-CREATE TABLE news_ratings 
+CREATE TABLE `rating` 
 (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    value INT NOT NULL CHECK (value >= 1 AND value <= 5),
-    user_id INT UNSIGNED NOT NULL,
-    news_id INT UNSIGNED NOT NULL,
-    ip_address VARCHAR(45) NOT NULL, -- Здесь можно хранить IP-адрес пользователя
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (news_id) REFERENCES news(id)
+    `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `value` INT NOT NULL CHECK (value >= 1 AND value <= 5),
+    `user_id` INT UNSIGNED NOT NULL,
+    `news_id` INT UNSIGNED NOT NULL,
+    `ip_address` VARCHAR(45) NOT NULL,
+    UNIQUE INDEX `unique_rating` (`user_id`, `news_id`, `ip_address`),
+    FOREIGN KEY (`user_id`) REFERENCES `user`(`id`),
+    FOREIGN KEY (`news_id`) REFERENCES `news`(`id`)
 );
