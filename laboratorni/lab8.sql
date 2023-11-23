0f2ec24d440a89565dabe412c6e034d0485f2f6c
qwistik <141317003+qwistik@users.noreply.github.com>
2023-11-23 13:23:48 +0100

    Update lab8.sql

--- laboratorni/lab8.sql
+++ laboratorni/lab8.sql
@@ -1,42 +1,51 @@
-CREATE TABLE users 
+CREATE TABLE `users` 
 (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-    password VARCHAR(50) NOT NULL
-    
+    `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+    `password` VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE projects
+CREATE TABLE `projects`
 (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    project_name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+    `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 );
 
-CREATE TABLE tasks
+CREATE TABLE `project_members` 
 (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-    project_id INT UNSIGNED,
-    author_id INT UNSIGNED,
-    FOREIGN KEY (author_id) REFERENCES users(id),
-    FOREIGN KEY (project_id) REFERENCES projects(id)
+    `project_id` INT UNSIGNED,
+    `user_id` INT UNSIGNED,
+    PRIMARY KEY (`project_id`, `user_id`),
+    FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
 );
 
-CREATE TABLE task_assignees
+CREATE TABLE `tasks`
 (
-    task_id INT UNSIGNED,
-    user_id INT UNSIGNED,
-    PRIMARY KEY (task_id, user_id),
-    FOREIGN KEY (task_id) REFERENCES tasks(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(1000) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+    `project_id` INT UNSIGNED,
+    `author_id` INT UNSIGNED,
+    FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`),
+    FOREIGN KEY (`author_id`) REFERENCES `users`(`id`)
 );
 
-CREATE TABLE files
+CREATE TABLE `task_assignees`
 (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-    task_id INT UNSIGNED NULL,
-    project_id INT UNSIGNED NULL,
-    FOREIGN KEY (task_id) REFERENCES tasks(id),
-    FOREIGN KEY (project_id) REFERENCES projects(id)
+    `task_id` INT UNSIGNED,
+    `user_id` INT UNSIGNED,
+    PRIMARY KEY (`task_id`, `user_id`),
+    FOREIGN KEY (`task_id`) REFERENCES `tasks`(`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
+);
+
+CREATE TABLE `files` 
+(
+    `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+    `task_id` INT UNSIGNED,
+    `project_id` INT UNSIGNED,
+    `file_url` VARCHAR(255) NOT NULL,
+    FOREIGN KEY (`task_id`) REFERENCES `tasks`(`id`),
+    FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`)
 );
